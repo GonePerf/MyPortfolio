b3f370a9c727e38523900a81a963241307c73928
Konrad Gumienny <konrad.gumienny@gmail.com>
2023-05-30 10:15:38 +0200

    Trying to configure SSL

--- mydjangobackend/settings.py
+++ mydjangobackend/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-_=0tzmd!sgorfvma0p545_5mv-ss$zee(v@vpgoedym9j19*t#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['www.kgdevelopment.pl', 'kgdevelopment.pl']
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
