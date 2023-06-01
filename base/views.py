779f3ba2a263bce38d16866fa124c7b91de1e6cb
Konrad Gumienny <konrad.gumienny@gmail.com>
2023-06-01 20:23:23 +0200

    Check status.

--- base/views.py
+++ base/views.py
@@ -1,41 +1,41 @@
-from django.shortcuts import render
-# from django.shortcuts import render, redirect
+# from django.shortcuts import render
+from django.shortcuts import render, redirect
 # from django.http import HttpResponse
-# from django.contrib import messages
-# from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 # from django.contrib.auth.decorators import login_required
 # from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 from .models import Tool, Topic, Message, Project
 # from .forms import ToolForm
 
-# def loginPage(request):
-#     page = 'login'
-#     if request.user.is_authenticated:
-#         return redirect('home')
+def loginPage(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('home')
 
-#     if request.method == 'POST':
-#         username = request.POST.get('username').lower()
-#         password = request.POST.get('password')
-#         try:
-#             user = User.objects.get(username=username)
-#         except:
-#             messages.error(request, 'User does not exist')
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
 
-#         user = authenticate(request, username=username, password=password)
+        user = authenticate(request, username=username, password=password)
 
-#         if user is not None:
-#             login(request, user)
-#             return redirect('home')
-#         else:
-#             messages.error(request, 'User with the credentials does not exist')
-#     context = { 'page': page }
-#     return render(request, 'base/views/login_register.html', context)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'User with the credentials does not exist')
+    context = { 'page': page }
+    return render(request, 'base/views/login_register.html', context)
 
-# def logoutUser(request):
-#     logout(request)
-#     return redirect('home')
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 
 # def registerUser(request):
 #     form = UserCreationForm()
